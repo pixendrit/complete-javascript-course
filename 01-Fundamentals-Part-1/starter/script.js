1e5dbda52c19074623d21c61058428d0eb983677
pixendrit <pixendrit@outlook.com>
2022-10-08 00:13:57 +0200

    ll

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -49,39 +49,111 @@ console.log(lastName);
 //not recommended. Will learn later why.
 */
 
-// Math operators
+/*
+ // Math operators
+ const now = 2037;
+ const ageJonas = now - 1991;
+ const ageSarah = now - 2018;
+ console.log(ageJonas, ageSarah);
+
+ //that is how we use the - operator to calculate. Also, console log can accept multiple values.
+
+ console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+ // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+ const firstName = 'Jonas';
+ const lastName = 'Schmedtmann';
+
+ console.log(firstName + ' ' + lastName);
+
+ //Assignment operators
+ let x = 10 + 5;
+ x += 10; //x = x + 10 = 25
+ x *= 4; //x = x * 4 = 100
+ x++; // x = x + 1
+ x--;
+ x--;
+ console.log(x);
+
+
+ // Comparision operators
+ console.log(ageJonas > ageSarah); // >, <, >=, <=
+ console.log(ageSarah >= 18);
+
+ const isFullAge = ageSarah >= 18;
+
+ console.log(now - 1991 > now - 2018);
+
+
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
 
-//that is how we use the - operator to calculate. Also, console log can accept multiple values.
-
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
-
-const firstName = 'Jonas';
-const lastName = 'Schmedtmann';
-
-console.log(firstName + ' ' + lastName);
-
-//Assignment operators
-let x = 10 + 5;
-x += 10; //x = x + 10 = 25
-x *= 4; //x = x * 4 = 100
-x++; // x = x + 1
-x--;
-x--;
-console.log(x);
+console.log(now - 1991, now - 2018);
 
 
-// Comparision operators
-console.log(ageJonas > ageSarah); // >, <, >=, <=
-console.log(ageSarah >= 18);
+let x, y;
+x = y = 25 - 10 - 5; //x = y = 10, x = 10
+console.log(x, y);
 
-const isFullAge = ageSarah >= 18;
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
 
-console.log(now - 1991 > now - 2018);
+
+// Coding Challenge #1
+// Mark and John are trying to compare their BMI (Body Mass Index), which is
+// calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+// and height in meter).
+// Your tasks:
+// 1. Store Mark's and John's mass and height in variables
+// 2. Calculate both their BMIs using the formula (you can even implement both
+// versions)
+// 3. Create a Boolean variable 'markHigherBMI' containing information about
+// whether Mark has a higher BMI than John.
+// Test data:
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+// m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+// m tall.
+// GOOD LUCK
+
+
+// 1
+const massMark = 95;
+const heightMark = 1.88;
+
+const massJohn = 85;
+const heightJohn = 1.76;
+
+//2
+const BMIMark = massMark / (heightMark * heightMark);
+
+
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+//3
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+*/
+
+
+const firstName = 'Pixendrit';
+const job = 'student';
+const birthYear = 1997;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
