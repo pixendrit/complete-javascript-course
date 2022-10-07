e4911104ab8f1735a0ac426c52ea98a91f45898b
pixendrit <pixendrit@outlook.com>
2022-10-07 11:12:22 +0200

    assignment2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,33 @@
 
-let js = "amazing";
+// let js = "amazing";
+// if (js === "amazing") alert("Javascript is FUN");
+// console.log(40 + 8 + 23 - 10);
 
-        if (js === "amazing") alert("Javascript is FUN");
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
 
-40+8+23-10;
-console.log(40 + 8 + 23 - 10);
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+//undefined. Declaring an empty value will have the value of undefined.
+
+year = 1991;
+console.log(typeof year);
+//not creating a new variable. Simply assigning a new value of the upper variable.
+//This is the effect of dynamic typing.
+
+console.log(typeof null);
+// it shows it is an object, but it is a bug that has not been fixed for legacy reasons.
+*/
+
+
+
