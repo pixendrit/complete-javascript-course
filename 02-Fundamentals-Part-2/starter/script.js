db2d10cae40bc776659c6cddbd304e94c0eae4d2
pixendrit <pixendrit@outlook.com>
2022-10-12 12:58:25 +0200

    strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Auto';
+// const private = 4234;
