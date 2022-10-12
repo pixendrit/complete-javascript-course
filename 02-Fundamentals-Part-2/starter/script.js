d5ddb0add291cefce90e3fc14cbe5e41310d1af0
pixendrit <pixendrit@outlook.com>
2022-10-12 14:17:49 +0200

    arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +9,62 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Auto';
 // const private = 4234;
+
+function logger() {
+console.log('My name is Pixendrit');
+}
+
+// calling / running / invoking function
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+//we need to save the return value. We need to capture it
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice)
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
+
+
+//function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+
+const age1 = calcAge1(1997);
+console.log(age1);
+
+//anonymous function. Function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+ */
+
+//Arrow functions. Easier and faster to write. Still a function expression though.
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3)
+
+// how many years a person has left untill retirement
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1997, 'Pixendrit'));
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+
