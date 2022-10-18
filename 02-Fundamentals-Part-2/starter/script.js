63726ae33f21a02cf403ee8b9fd3332c306ae1f6
pixendrit <pixendrit@outlook.com>
2022-10-18 11:34:35 +0200

    loop inside a loop very nice concept

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -496,5 +496,57 @@ for (let i = 0; i < jonasArray.length; i++) {
 // Looping Backwards and Loops in Loops
 
 
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(jonas[i]);
+}
+
+// loop inside a loop
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`----------------- Starting exercisee ${exercise}`);
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Liftin weight repetition ${rep}`)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
